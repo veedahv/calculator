bab1414b14c8a7396dee453ba6c3e0b94f925006
veedahv <veedahv55@gmail.com>
2021-12-01 10:54:26 +0100

    code formatted

--- js/script.js
+++ js/script.js
@@ -2,13 +2,26 @@ const numericalKeys = document.querySelectorAll('.numerical-keys');
 const operationKeys = document.querySelectorAll('.operation-keys');
 const screenInput = document.querySelector('#screen-input');
 const screenOutput = document.querySelector('#screen-output');
+const toast = document.querySelector('#toast');
 let calcOperation;
-let noOfOpenBrackets = 0;
-let noOfClosedBrackets = 0;
-let noOfMissingBrackets = noOfOpenBrackets - noOfClosedBrackets;
+let noOfMissingBrackets = 0;
+const showToast = (mssg) => {
+    toast.textContent = mssg;
+    toast.classList.add('show');
+    setTimeout(() => {
+        toast.classList.remove('show');
+    }, 500);
+}
+const getCalcOperation = (newOperation) => {
+    calcOperation = newOperation.split('+').join('+').split('−').join('-').split('×').join('*').split('÷').join('/').split('%').join('/100');
+    let noOfOpenBrackets = calcOperation.replace(/[^(]/g, "").length;
+    let noOfClosedBrackets = calcOperation.replace(/[^)]/g, "").length;
+    noOfMissingBrackets = noOfOpenBrackets - noOfClosedBrackets;
+    calcOperation = calcOperation + ')'.repeat(noOfMissingBrackets);
+}
 const addNewValue = (newOperand) => {
     if (screenInput.innerHTML.length > 200) {
-        console.log("can't enter more than 200 characters");
+        showToast("Can't enter more than 200 characters");
     } else {
         let inputSelectionStart = screenInput.selectionStart;
         let inputSelectionEnd = screenInput.selectionEnd;
@@ -16,6 +29,7 @@ const addNewValue = (newOperand) => {
         let currentOperation = screenInput.innerHTML;
         let currentOperationArray = currentOperation.split("");
         let re = /[+÷×−]/;
+        let reg = /[%)]/;
         let prevValue = currentOperationArray.slice(inputSelectionStart - 1, inputSelectionStart).join('');
         let nextValue = currentOperationArray.slice(inputSelectionEnd, inputSelectionEnd + 1).join('');
         if (re.test(prevValue) && re.test(newOperand)) {
@@ -24,13 +38,12 @@ const addNewValue = (newOperand) => {
         } else if (re.test(nextValue) && re.test(newOperand)) {
             valueReplaced = 1;
         }
+        if (reg.test(prevValue) && !re.test(newOperand)) {
+            newOperand = '×' + newOperand;
+        }
         currentOperationArray.splice(inputSelectionStart, valueReplaced, newOperand);
         let newOperation = currentOperationArray.join("");
-        calcOperation = newOperation.split('+').join('+').split('−').join('-').split('×').join('*').split('÷').join('/').split('%').join('/100');
-        noOfOpenBrackets = calcOperation.replace(/[^(]/g, "").length;
-        noOfClosedBrackets = calcOperation.replace(/[^)]/g, "").length;
-        noOfMissingBrackets = noOfOpenBrackets - noOfClosedBrackets;
-        calcOperation = calcOperation + ')'.repeat(noOfMissingBrackets);
+        getCalcOperation(newOperation);
         inputSelectionStart += newOperand.length;
         console.log(newOperand.length);
         screenInput.innerHTML = newOperation;
@@ -44,7 +57,13 @@ operationKeys.forEach(operationKey => {
             screenOutput.value = '';
             screenInput.innerText = '';
         } else if (operationKey.dataset.action === 'equate') {
-            screenInput.innerText = eval(calcOperation);
+            try {
+                screenInput.innerText = eval(calcOperation);
+            } catch (e) {
+                if (e instanceof SyntaxError) {
+                    showToast('Invalid Format');
+                }
+            }
             screenOutput.value = '';
         } else if (operationKey.dataset.action === 'backspace') {
             let inputSelectionStart = screenInput.selectionStart - 1;
@@ -54,7 +73,7 @@ operationKeys.forEach(operationKey => {
             let currentOperationArray = currentOperation.split("");
             currentOperationArray.splice(inputSelectionStart, valueReplaced);
             let newOperation = currentOperationArray.join("");
-            calcOperation = newOperation.split('+').join('+').split('−').join('-').split('×').join('*').split('÷').join('/').split('%').join('/100');
+            getCalcOperation(newOperation);
             inputSelectionStart;
             screenInput.innerHTML = newOperation;
             screenInput.focus();
@@ -74,8 +93,8 @@ operationKeys.forEach(operationKey => {
                     let reg = new RegExp('^[0-9)]$');
                     if (reg.test(prevValue) && reg.test(nextValue) && noOfMissingBrackets > 0) {
                         addNewValue(')×');
-                        } else if (reg.test(prevValue) && noOfMissingBrackets > 0) {
-                            addNewValue(')');
+                    } else if (reg.test(prevValue) && noOfMissingBrackets > 0) {
+                        addNewValue(')');
                     } else {
                         addNewValue('×(');
                     }
@@ -94,54 +113,46 @@ operationKeys.forEach(operationKey => {
     })
 });
 function getValuePosition(str, position) {
-    const n = str.substring(position).match(/^[0-9.%]+/);
-    const p = str.substring(0, position).match(/[0-9.%]+$/);
+    const n = str.substring(position).match(/^[0-9.%]/);
+    const p = str.substring(0, position).match(/[0-9.%]$/);
     return !p && !n ? '' : (p || '') + (n || '');
 }
 numericalKeys.forEach(numericalKey => {
     numericalKey.addEventListener('click', () => {
+        let inputSelectionStart = screenInput.selectionStart;
+        let currentOperation = screenInput.innerHTML;
+        let currentOperationArray = currentOperation.split("");
+        let selectValue = getValuePosition(screenInput.innerHTML, screenInput.selectionStart);
         if (numericalKey.dataset.action === 'negation') {
-            console.log('high');
-            let inputSelectionStart = screenInput.selectionStart;
-            // let inputSelectionEnd = screenInput.selectionEnd;
-            let currentOperation = screenInput.innerHTML;
-            let currentOperationArray = currentOperation.split("");
-            let selectValue = getValuePosition(screenInput.innerHTML, screenInput.selectionStart);
             let valueIndex = currentOperation.indexOf(selectValue);
-            let prevValues = currentOperationArray.slice(valueIndex - 2, valueIndex).join('');
             console.log(valueIndex);
-            console.log(prevValues);
+            let prevValues = (selectValue === '') ? currentOperationArray.slice(inputSelectionStart - 2, inputSelectionStart).join('') : currentOperationArray.slice(valueIndex - 2, valueIndex).join('');
             if (prevValues === '(−') {
-                currentOperationArray.splice(valueIndex - 2, 2, '');
-                console.log('negated');
+                (selectValue === '') ? currentOperationArray.splice(inputSelectionStart - 2, 2, '') : currentOperationArray.splice(valueIndex - 2, 2, '');
                 inputSelectionStart -= 2;
             } else {
-                currentOperationArray.splice(valueIndex, 0, '(−');
-                console.log('not negated');
-                inputSelectionStart += 2;
+                console.log(selectValue);
+                (selectValue === '') ? currentOperationArray.splice(inputSelectionStart, 0, '(−') : currentOperationArray.splice(valueIndex, 0, '(−');
+                 inputSelectionStart += 2;
             }
             let newOperation = currentOperationArray.join("");
             screenInput.innerHTML = newOperation;
             screenInput.focus();
             screenInput.selectionStart = inputSelectionStart;
         } else if (numericalKey.dataset.action === 'decimal') {
-            let inputSelectionStart = screenInput.selectionStart;
-            // let inputSelectionEnd = screenInput.selectionEnd;
-            let valueReplaced = screenInput.selectionEnd - screenInput.selectionStart;
-            let currentOperation = screenInput.innerHTML;
-            let currentOperationArray = currentOperation.split("");
-            let selectValue = getValuePosition(screenInput.innerHTML, screenInput.selectionStart);
             if (!selectValue.includes('.')) {
-                currentOperationArray.splice(inputSelectionStart, valueReplaced, '.');
-                let newOperation = currentOperationArray.join("");
-                inputSelectionStart++;
-                screenInput.innerHTML = newOperation;
-                screenInput.focus();
-                screenInput.selectionStart = inputSelectionStart;
+                addNewValue('.');
             }
+            screenInput.focus();
         } else {
             addNewValue(numericalKey.innerText);
-            screenOutput.value = eval(calcOperation);
+            try {
+                screenOutput.value = eval(calcOperation);
+            } catch (e) {
+                if (e instanceof SyntaxError) {
+                    showToast('Invalid Format');
+                }
+            }
         }
     })
 });
